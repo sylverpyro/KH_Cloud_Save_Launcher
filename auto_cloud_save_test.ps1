a2a55b1131e89f4bcc1d844d70360ad7ccf12f1a
sylverpyro <sylverpyro@users.noreply.github.com>
2021-04-23 17:52:52 -0500

    First pass at re-write
    First glance coverage complete - definitely needs testing and checking

--- auto_cloud_save_test.ps1
+++ auto_cloud_save_test.ps1
@@ -2,12 +2,24 @@
 $local_save_dir = '~\Documents\KINGDOM HEARTS HD 1.5+2.5 ReMIX\Epic Games Store\0d8153b07f0043778a5d3fc0a6f6f6c7'
 $local_save = "$local_save_dir\$game_save_file"
 # Cloud save file DIR
-$cloud_save_dir = '~\Google Drive\Cloud Saves\Manual - KH'
+$cloud_save_dir = '~\Google Drive\Cloud Saves\Kingdom Hearts 1.5+2.5 ReMix'
 # Save file name (as the game expects and creates it)
-$game_save_file = 'KHFM.png'
+$KHFM_save_file = 'KHFM.png'
+$KHCOM_save_file = 'KHCoM.png'
 # Backup save folder name
 $save_backups = 'SaveBackups'
 
+
+Function _cloud_sync_file($save_file){
+
+    Write-Output "Got file: $save_file"
+    $folder_name = $save_file.TrimEnd(".png")
+    Write-Output "Basename: $folder_name"
+    if ( -not(Test-Path -Path $cloud_save_dir/$folder_name -PathType Container)) {
+        Write-Output "Did not detect '$cloud_save_dir/$folder_name' want to create"
+    }
+
+}
 Function _cloud_sync {
 
     if ( Test-Path -Path $cloud_save_dir -PathType Container ) {
@@ -34,16 +46,19 @@ Function _cloud_sync {
     if ( $newest_cloud_ts -gt $local_save_ts ) {
         Write-Output "Cloud is newer than local - Want to copy to local"
         Write-Output "  Would: Test-Path $local_save_dir/$save_backups"
-        Write-Output "  Would: mv $local_save_dir/$game_save_file $local_save_dir/$save_backups/KHFM-$local_save_ts.png"
+        Write-Output "  Would: mv $local_save_dir/$KHFM_save_file $local_save_dir/$save_backups/KHFM-$local_save_ts.png"
         Write-Output "  Would: cp $cloud_save_dir/$newest_cloud $local_save_dir"
     }
     elseif ( $local_save_ts -gt $newest_cloud_ts ) {
         Write-Output "Local is newer than cloud - want to copy to cloud"
-        Write-Output "  Would: cp $local_save_dir/$game_save_file $cloud_save_dir/KHFM-$local_save_ts.png"
+        Write-Output "  Would: cp $local_save_dir/$KHFM_save_file $cloud_save_dir/KHFM-$local_save_ts.png"
     }
     elseif ( $local_save_ts -eq $newest_cloud_ts) {
         Write-Output "Local is the same as cloud - nothing to do"
     }
 }
 
-_cloud_sync
+foreach ($save in @($KHFM_save_file, $KHCOM_save_file)) {
+    #Write-Output "Got save: $save"
+    _cloud_sync_file $save
+}
